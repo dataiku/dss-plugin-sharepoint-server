a24fdb18e4323a6c8eaf77cd55af3c4424c132e2
Alexandre Bourret <alexandre.bourret@dataiku.com>
2020-02-24 11:53:39 +0100

    Implementing comments by Marc, Mayeul and Clement

--- python-lib/dss_constants.py
+++ python-lib/dss_constants.py
@@ -1,18 +1,19 @@
-APPLICATION_JSON = "application/json;odata=verbose"
-APPLICATION_JSON_NOMETADATA = "application/json; odata=nometadata"
-DSS_PATH = 'path'
-DSS_FULL_PATH = 'fullPath'
-DSS_EXISTS = 'exists'
-DSS_DIRECTORY = 'directory'
-DSS_IS_DIRECTORY = 'isDirectory'
-DSS_SIZE = 'size'
-DSS_LAST_MODIFIED = 'lastModified'
-DSS_CHILDREN = 'children'
-AUTH_OAUTH = "oauth"
-AUTH_LOGIN = "login"
-DSS_LOCAL_DETAILS = {
-    "sharepoint_username" : "The account's username is missing",
-    "sharepoint_password" : "The account's password is missing",
-    "sharepoint_host" : "The SharePoint host address is missing",
-    "sharepoint_site" : "The site name is missing"
-}
+class DSSConstants(object):
+    APPLICATION_JSON = "application/json;odata=verbose"
+    APPLICATION_JSON_NOMETADATA = "application/json; odata=nometadata"
+    PATH = 'path'
+    FULL_PATH = 'fullPath'
+    EXISTS = 'exists'
+    DIRECTORY = 'directory'
+    IS_DIRECTORY = 'isDirectory'
+    SIZE = 'size'
+    LAST_MODIFIED = 'lastModified'
+    CHILDREN = 'children'
+    AUTH_OAUTH = "oauth"
+    AUTH_LOGIN = "login"
+    LOCAL_DETAILS = {
+        "sharepoint_username": "The account's username is missing",
+        "sharepoint_password": "The account's password is missing",
+        "sharepoint_host": "The SharePoint host address is missing",
+        "sharepoint_site": "The site name is missing"
+    }
--- python-lib/sharepoint_client.py
+++ python-lib/sharepoint_client.py
@@ -1,19 +1,17 @@
-import os, requests, sharepy
+import os
+import requests
+
 from requests_ntlm import HttpNtlmAuth
 
-try:
-    from BytesIO import BytesIO ## for Python 2
-except ImportError:
-    from io import BytesIO ## for Python 3
+from sharepoint_constants import SharePointConstants
+from dss_constants import DSSConstants
 
-from sharepoint_constants import *
-from dss_constants import *
 
 class SharePointClient():
 
     def __init__(self, config):
         login_details = config.get('sharepoint_local')
-        self.assert_login_details(DSS_LOCAL_DETAILS, login_details)
+        self.assert_login_details(DSSConstants.LOCAL_DETAILS, login_details)
         self.setup_login_details(login_details)
         username = login_details['sharepoint_username']
         password = login_details['sharepoint_password']
@@ -26,7 +24,7 @@ class SharePointClient():
             self.sharepoint_origin,
             self.sharepoint_type,
             self.sharepoint_site,
-            sharepoint_access_token = None
+            sharepoint_access_token=None
         )
         self.sharepoint_list_title = config.get("sharepoint_list_title")
 
@@ -42,12 +40,12 @@ class SharePointClient():
             self.sharepoint_root = "Shared Documents"
 
     def get_folders(self, path):
-        response = self.session.get(self.get_sharepoint_item_url(path) + "/Folders" )
+        response = self.session.get(self.get_sharepoint_item_url(path) + "/Folders")
         self.assert_response_ok(response)
         return response.json()
 
     def get_files(self, path):
-        response = self.session.get(self.get_sharepoint_item_url(path) + "/Files" )
+        response = self.session.get(self.get_sharepoint_item_url(path) + "/Files")
         self.assert_response_ok(response)
         return response.json()
 
@@ -98,21 +96,21 @@ class SharePointClient():
 
     def delete_file(self, full_path):
         headers = {
-            "X-HTTP-Method":"DELETE"
+            "X-HTTP-Method": "DELETE"
         }
         response = self.session.post(
             self.get_file_url(full_path),
-            headers = headers
+            headers=headers
         )
         self.assert_response_ok(response)
 
     def delete_folder(self, full_path):
         headers = {
-            "X-HTTP-Method":"DELETE"
+            "X-HTTP-Method": "DELETE"
         }
         response = self.session.post(
             self.get_folder_url(full_path),
-            headers = headers
+            headers=headers
         )
         self.assert_response_ok(response)
 
@@ -126,9 +124,11 @@ class SharePointClient():
     def get_list_all_items(self, list_title):
         items = self.get_list_items(list_title)
         buffer = items
-        while SHAREPOINT_RESULTS_CONTAINER_V2 in items and SHAREPOINT_NEXT_PAGE in items[SHAREPOINT_RESULTS_CONTAINER_V2]:
-            items = self.session.get(items[SHAREPOINT_RESULTS_CONTAINER_V2][SHAREPOINT_NEXT_PAGE]).json()
-            buffer[SHAREPOINT_RESULTS_CONTAINER_V2][SHAREPOINT_RESULTS].extend(items[SHAREPOINT_RESULTS_CONTAINER_V2][SHAREPOINT_RESULTS])
+        while SharePointConstants.RESULTS_CONTAINER_V2 in items and SharePointConstants.NEXT_PAGE in items[SharePointConstants.RESULTS_CONTAINER_V2]:
+            items = self.session.get(items[SharePointConstants.RESULTS_CONTAINER_V2][SharePointConstants.NEXT_PAGE]).json()
+            buffer[SharePointConstants.RESULTS_CONTAINER_V2][SharePointConstants.RESULTS].extend(
+                items[SharePointConstants.RESULTS_CONTAINER_V2][SharePointConstants.RESULTS]
+            )
         return buffer
 
     def get_list_items(self, list_title):
@@ -139,8 +139,8 @@ class SharePointClient():
         return response.json()
 
     def create_list(self, list_name):
-        headers={
-            "content-type": APPLICATION_JSON,
+        headers = {
+            "content-type": DSSConstants.APPLICATION_JSON,
             'Accept': 'application/json; odata=nometadata'
         }
         data = {
@@ -161,29 +161,29 @@ class SharePointClient():
         return response
 
     def delete_list(self, list_name):
-        headers={
+        headers = {
             "X-HTTP-Method": "DELETE",
-            "IF-MATCH" : "*"
+            "IF-MATCH": "*"
         }
         response = self.session.post(
             self.get_lists_by_title_url(list_name),
-            headers = headers
+            headers=headers
         )
         return response
 
     def create_custom_field(self, list_title, field_title):
         body = {
-            'parameters' : {
-                '__metadata': { 'type': 'SP.XmlSchemaFieldCreationInformation' },
-                'SchemaXml':"<Field DisplayName='{0}' Format='Dropdown' MaxLength='255' Name='{0}' Title='{0}' Type='Text'></Field>".format(field_title)
+            'parameters': {
+                '__metadata': {'type': 'SP.XmlSchemaFieldCreationInformation'},
+                'SchemaXml': "<Field DisplayName='{0}' Format='Dropdown' MaxLength='255' Name='{0}' Title='{0}' Type='Text'></Field>".format(field_title)
             }
         }
         headers = {
-            "content-type": APPLICATION_JSON
+            "content-type": DSSConstants.APPLICATION_JSON
         }
         response = self.session.post(
             self.get_lists_add_field_url(list_title),
-            headers = headers,
+            headers=headers,
             json=body
         )
         self.assert_response_ok(response)
@@ -191,10 +191,10 @@ class SharePointClient():
 
     def add_list_item(self, list_title, item):
         item["__metadata"] = {
-            "type" : "SP.Data.{}ListItem".format(list_title.capitalize())
+            "type": "SP.Data.{}ListItem".format(list_title.capitalize())
         }
         headers = {
-            "Content-Type": APPLICATION_JSON
+            "Content-Type": DSSConstants.APPLICATION_JSON
         }
         response = self.session.post(
             self.get_list_items_url(list_title),
@@ -238,7 +238,7 @@ class SharePointClient():
             self.get_site_path(full_path)
         )
 
-    def get_file_content_url(self,full_path):
+    def get_file_content_url(self, full_path):
         return self.get_file_url(full_path) + "/$value"
 
     def get_move_url(self, from_path, to_path):
@@ -271,29 +271,31 @@ class SharePointClient():
         if status_code == 403:
             raise Exception("Forbidden. Please check your account credentials.")
 
+
 class SharePointSession():
 
-    def __init__(self, sharepoint_user_name, sharepoint_password, sharepoint_tenant, sharepoint_site, sharepoint_access_token = None):
+    def __init__(self, sharepoint_user_name, sharepoint_password, sharepoint_tenant, sharepoint_site, sharepoint_access_token=None):
         self.sharepoint_tenant = sharepoint_tenant
         self.sharepoint_site = sharepoint_site
         self.sharepoint_access_token = sharepoint_access_token
 
-    def get(self, url, headers = {}):
-        headers["accept"] = APPLICATION_JSON
+    def get(self, url, headers={}):
+        headers["accept"] = DSSConstants.APPLICATION_JSON
         headers["Authorization"] = self.get_authorization_bearer()
-        return requests.get(url, headers = headers)
+        return requests.get(url, headers=headers)
 
-    def post(self, url, headers = {}, json=None, data=None):
-        headers["accept"] = APPLICATION_JSON
+    def post(self, url, headers={}, json=None, data=None):
+        headers["accept"] = DSSConstants.APPLICATION_JSON
         headers["Authorization"] = self.get_authorization_bearer()
-        return requests.post(url, headers = headers, json=json, data=data)
+        return requests.post(url, headers=headers, json=json, data=data)
 
     def get_authorization_bearer(self):
         return "Bearer {}".format(self.sharepoint_access_token)
 
+
 class LocalSharePointSession():
 
-    def __init__(self, sharepoint_user_name, sharepoint_password, sharepoint_origin, sharepoint_type, sharepoint_site, sharepoint_access_token = None):
+    def __init__(self, sharepoint_user_name, sharepoint_password, sharepoint_origin, sharepoint_type, sharepoint_site, sharepoint_access_token=None):
         self.form_digest_value = None
         self.sharepoint_origin = sharepoint_origin
         self.sharepoint_type = sharepoint_type
@@ -303,24 +305,24 @@ class LocalSharePointSession():
         self.sharepoint_password = sharepoint_password
         self.auth = HttpNtlmAuth(sharepoint_user_name, sharepoint_password)
 
-    def get(self, url, headers = {}):
-        headers["accept"] = APPLICATION_JSON
-        return requests.get(url, headers = headers, auth=self.auth)
+    def get(self, url, headers={}):
+        headers["accept"] = DSSConstants.APPLICATION_JSON
+        return requests.get(url, headers=headers, auth=self.auth)
 
-    def post(self, url, headers = {}, json=None, data=None):
-        headers["accept"] = APPLICATION_JSON
+    def post(self, url, headers={}, json=None, data=None):
+        headers["accept"] = DSSConstants.APPLICATION_JSON
         headers["X-RequestDigest"] = self.get_form_digest_value()
-        return requests.post(url, headers = headers, json=json, data=data, auth=self.auth)
+        return requests.post(url, headers=headers, json=json, data=data, auth=self.auth)
 
     def get_form_digest_value(self):
         if self.form_digest_value is not None:
             return self.form_digest_value
-        headers={}
-        headers["accept"] = APPLICATION_JSON_NOMETADATA
-        response = requests.post(self.get_context_info_url(), headers = headers, auth=self.auth)
+        headers = {}
+        headers["accept"] = DSSConstants.APPLICATION_JSON_NOMETADATA
+        response = requests.post(self.get_context_info_url(), headers=headers, auth=self.auth)
         json_response = response.json()
-        if SHAREPOINT_FORM_DIGEST_VALUE in json_response:
-            self.form_digest_value = json_response[SHAREPOINT_FORM_DIGEST_VALUE]
+        if SharePointConstants.FORM_DIGEST_VALUE in json_response:
+            self.form_digest_value = json_response[SharePointConstants.FORM_DIGEST_VALUE]
             return self.form_digest_value
 
     def get_context_info_url(self):
