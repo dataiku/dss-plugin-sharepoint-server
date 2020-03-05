5cb33ff92ef701e7c3c5e011050c376d92f5fcac
Alexandre Bourret <alexandre.bourret@dataiku.com>
2020-03-05 16:28:33 +0100

    Fix root folder bug, input box for root folder, improved error messages

--- python-lib/sharepoint_client.py
+++ python-lib/sharepoint_client.py
@@ -67,7 +67,7 @@ class SharePointClient():
         response = self.session.get(
             self.get_file_content_url(full_path)
         )
-        self.assert_response_ok(response)
+        self.assert_response_ok(response, no_json=True)
         return response
 
     def write_file_content(self, full_path, data):
@@ -255,10 +255,11 @@ class SharePointClient():
         )
 
     def get_site_path(self, full_path):
-        return "'/{}/{}/Shared%20Documents{}'".format(self.sharepoint_type, self.sharepoint_site, full_path)
+        return "'/{}/{}/{}{}'".format(self.sharepoint_type, self.sharepoint_site, self.sharepoint_root, full_path)
 
     def get_add_folder_url(self, full_path):
-        return self.get_base_url() + "/Folders/add('Shared%20Documents/{}')".format(
+        return self.get_base_url() + "/Folders/add('{}/{}')".format(
+            self.sharepoint_root,
             full_path
         )
 
@@ -272,12 +273,21 @@ class SharePointClient():
             if key not in login_details.keys():
                 raise Exception(required_keys[key])
 
-    def assert_response_ok(self, response):
+    def assert_response_ok(self, response, no_json=False):
         status_code = response.status_code
         if status_code == 404:
             raise Exception("Not found. Please check tenant, site type or site name.")
         if status_code == 403:
             raise Exception("Forbidden. Please check your account credentials.")
+        if not no_json:
+            if len(response.content) == 0:
+                raise Exception("Empty response from SharePoint. Please check user credentials.")
+            json_response = response.json()
+            if "error" in json_response:
+                if "message" in json_response["error"] and "value" in json_response["error"]["message"]:
+                    raise Exception("Error: {}".format(json_response["error"]["message"]["value"]))
+                else:
+                    raise Exception("Error")
 
 
 class SharePointSession():
